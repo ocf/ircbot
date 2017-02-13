7bf04fe703f00982c0fa28d79423af40feae0c74
Jason Perrin <jvperrin@ocf.berkeley.edu>
2017-02-12 23:34:26 -0800

    Revert 'Extract regex constants'
    This reverts a268ad3e59945.
    
    (but also keeps the '@' character, because that's still good)

--- ircbot/ircbot.py
+++ ircbot/ircbot.py
@@ -31,10 +31,6 @@ IRC_PORT = 6697
 IRC_CHANNELS = ('#rebuild', '#atool')
 IRC_CHANNELS_ANNOUNCE = ('#atool',)
 
-RT = re.compile('rt#([0-9]+)')
-REPLACE = re.compile('(?:^| )s([!@"#$%&\'*./:;=?\\^_`|~])(.+)\1(.*)\1g?$')
-SHRUG = re.compile('s+h+r+(u+)g+')
-
 user = getpass.getuser()
 if user == 'nobody':
     IRC_NICKNAME = 'create'
@@ -90,9 +86,10 @@ class CreateBot(irc.bot.SingleServerIRCBot):
                 conn.privmsg(event.target, fmt.format(user=user, msg=msg))
 
             # maybe do something with it
-            tickets = RT.findall(msg)
-            replacement = REPLACE.search(msg)
-            shrug = SHRUG.search(msg)
+            tickets = re.findall(r'rt#([0-9]+)', msg)
+            replace = r'(?:^| )s([!"#$%&\'*./:;=?\\^_`|~])(.+)\1(.*)\1g?$'
+            replacement = re.search(replace, msg)
+            shrug = re.search(r's+h+r+(u+)g+', msg)
             if tickets:
                 rt = rt_connection(user='create', password=self.rt_password)
                 for ticket in tickets:
