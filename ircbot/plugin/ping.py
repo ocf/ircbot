42a959e0f63d644ed613bd8eb74c7ce318d1bea5
kkuehler <kkuehler@berkeley.edu>
2017-12-21 15:02:18 -0800

    Clean up case insensitive ping

--- ircbot/plugin/ping.py
+++ ircbot/plugin/ping.py
@@ -1,18 +1,18 @@
 """Check your connection."""
 import random
+import re
 
 
 def register(bot):
-    bot.listen(r'^[Pp]+[Ii]+[Nn]+[Gg]+$', pong, require_mention=True)
+    bot.listen(r'^p+i+n+g+$', pong, flags=re.IGNORECASE, require_mention=True)
 
 
 def pong(bot, msg):
     pleased = random.randint(0, 99)
     if pleased:
         msg.respond(
-            ''.join([chr(ord(c) + 6)
-                     if c.lower() == 'i' else c
-                     for c in msg.text]),
-            ping=False)
+            msg.text.replace('i', 'o').replace('I', 'O'),
+            ping=False,
+        )
     else:
         msg.respond('not now, please.', ping=False)
