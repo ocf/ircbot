ab7c0de7ce08a1cbb9ea35b119601e9a10f016c5
kkuehler <kkuehler@berkeley.edu>
2017-12-21 00:24:45 -0800

    make ping case insensitive

--- ircbot/plugin/ping.py
+++ ircbot/plugin/ping.py
@@ -3,12 +3,16 @@ import random
 
 
 def register(bot):
-    bot.listen(r'^p+i+n+g+$', pong, require_mention=True)
+    bot.listen(r'^[Pp]+[Ii]+[Nn]+[Gg]+$', pong, require_mention=True)
 
 
 def pong(bot, msg):
     pleased = random.randint(0, 99)
     if pleased:
-        msg.respond(msg.text.replace('i', 'o'), ping=False)
+        msg.respond(
+            ''.join([chr(ord(c) + 6)
+                     if c.lower() == 'i' else c
+                     for c in msg.text]),
+            ping=False)
     else:
         msg.respond('not now, please.', ping=False)
