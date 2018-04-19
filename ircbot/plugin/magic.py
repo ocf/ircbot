2a4bf2b7a411be6026b387d2d5b4780176243805
Kevin Peng <kpengboy@ocf.berkeley.edu>
2018-04-18 21:37:54 -0700

    magic: add more query regexes

--- ircbot/plugin/magic.py
+++ ircbot/plugin/magic.py
@@ -3,7 +3,9 @@
 
 def register(bot):
     bot.listen(r'^magic ?(.*)$', magic, require_mention=True)
-    bot.listen(r"\bmystery\b|^why (doesn't it work|isn't it working)$|^how does it work$", mystery)
+    bot.listen(r'\bmystery\b|'
+               r"^why (doesn't it work|isn't it working)$|"
+               r'^how does (it|anything) (even )?work$', mystery)
 
 
 def _magic(thing):
