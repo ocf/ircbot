3563a9a1e167634d0774b288e0220eaf97152025
Jason Perrin <jvperrin@ocf.berkeley.edu>
2018-07-06 15:31:19 -0700

    what_is: respond with are/is correctly

--- ircbot/plugin/what_is.py
+++ ircbot/plugin/what_is.py
@@ -3,13 +3,13 @@ from ircbot import db
 
 
 def register(bot):
-    bot.listen(r'^(what|who) (?:is|are) (.+)$', what_is, require_mention=True)
-    bot.listen(r'^know that (.+?) (?:is|are) (.+)$', know_that, require_mention=True)
+    bot.listen(r'^(what|who) (is|are) (.+)$', what_is, require_mention=True)
+    bot.listen(r'^know that (.+?) (is|are) (.+)$', know_that, require_mention=True)
 
 
 def what_is(bot, msg):
     """Print out the current definition."""
-    what_who, thing = msg.match.groups()
+    what_who, is_are, thing = msg.match.groups()
 
     # Special case: "who is in the lab" should be ignored
     if thing.startswith('in the lab'):
@@ -27,17 +27,17 @@ def what_is(bot, msg):
         definition = c.fetchone()
 
     if definition is not None:
-        msg.respond('{} is {}'.format(thing, definition['what_it_is']), ping=False)
+        msg.respond('{} {} {}'.format(thing, is_are, definition['what_it_is']), ping=False)
     else:
-        msg.respond('idk {} {} is'.format(what_who, thing), ping=False)
+        msg.respond('idk {} {} {}'.format(what_who, thing, is_are), ping=False)
 
 
 def know_that(bot, msg):
     """Create or update a definition."""
-    thing, what_it_is = msg.match.groups()
+    thing, is_are, what_it_is = msg.match.groups()
     with db.cursor(password=bot.mysql_password) as c:
         c.execute(
             'REPLACE INTO what_is (thing, what_it_is) VALUES (%s, %s)',
             (thing, what_it_is),
         )
-    msg.respond('okay, {} is {}'.format(thing, what_it_is), ping=False)
+    msg.respond('okay, {} {} {}'.format(thing, is_are, what_it_is), ping=False)
