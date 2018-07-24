46577de70b408d7356daace8052c2da4ccb89778
Kevin Peng <kpengboy@ocf.berkeley.edu>
2018-07-24 02:33:37 -0700

    magic: eliminate need for mention in some conditions

--- ircbot/plugin/magic.py
+++ ircbot/plugin/magic.py
@@ -4,10 +4,8 @@
 def register(bot):
     bot.listen(r'^magic ?(.*)$', magic, require_mention=True)
     bot.listen(r'\bmystery\b|'
-               r"\bwhy (doesn't it work|isn't it working)\b|"
-               r'\bhow does (it|anything) (even )?work\b', mystery)
-    bot.listen(r"^why (do(es)?n't .+ work|(is|are)n't .+ working)$|"
-               r'^how do(es)? .+ work$', mystery, require_mention=True)
+               r"\bwhy (do(es)?n't .+ work|(is|are)n't .+ working)\b|"
+               r'\bhow do(es)? .+ work\b', mystery)
 
 
 def _magic(thing):
