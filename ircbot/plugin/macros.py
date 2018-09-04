c24ee2f1666c937532374851fd86019670495552
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2018-09-03 23:51:57 -0700

    add phabricator-like macros to ircbot

---
+++ ircbot/plugin/macros.py
@@ -0,0 +1,64 @@
+"""Provide OCF image macros, inspired by Phabricator Macros"""
+from ircbot import db
+
+
+def register(bot):
+    bot.listen(r'^#m (\w+)$', show)
+    bot.listen(r'#m add (\w+) (.+)$', add)
+    bot.listen(r'#m delete (\w+)$', delete)
+
+
+def show(bot, msg):
+    """Return a macro by slug."""
+    slug = msg.match.group(1)
+
+    with db.cursor(password=bot.mysql_password) as c:
+        c.execute(
+            'SELECT link FROM macros WHERE slug = %s',
+            (slug,)
+        )
+        macro = c.fetchone()
+        if macro is not None:
+            msg.respond(macro['link'], ping=False)
+        else:
+            msg.respond('macro `{}` does not exist.'.format(slug))
+
+
+def add(bot, msg):
+    """Add a new macro."""
+
+    slug = msg.match.group(1)
+    link = msg.match.group(2)
+
+    if len(link) > 80:
+        msg.respond('please try to keep macro links below 80 characters')
+
+    with db.cursor(password=bot.mysql_password) as c:
+
+        c.execute('SELECT * FROM macros WHERE slug = %s', (slug,))
+        result = c.fetchone()
+        if result is not None:
+            msg.respond(
+                'macro `{}` already exists as {}'.format(
+                    result['slug'],
+                    result['link'],
+                )
+            )
+        else:
+            c.execute(
+                'INSERT INTO macros (slug, link) VALUES (%s, %s)',
+                (slug, link)
+            )
+            msg.respond('macro added as `{}`'.format(slug))
+
+
+def delete(bot, msg):
+    """Delete a macro."""
+
+    slug = msg.match.group(1)
+    with db.cursor(password=bot.mysql_password) as c:
+        c.execute(
+            'DELETE FROM macros WHERE slug = %s',
+            (slug,)
+        )
+        msg.respond('macro `{}` has been deleted.'.format(slug))
