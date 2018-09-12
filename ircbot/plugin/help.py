0b304602b23c94c124c36b2980a7fdb5fb1bbaec
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2018-09-12 16:05:16 -0700

    add help page listing available macros

--- ircbot/plugin/help.py
+++ ircbot/plugin/help.py
@@ -9,11 +9,17 @@ import jinja2
 def register(bot):
     threading.Thread(target=help_server, args=(bot,), daemon=True).start()
     bot.listen(r'^help$', help, require_mention=True)
+    bot.listen(r'^macros$', help_macro, require_mention=True)
 
 
 def help(bot, msg):
     """Provide a link to this help page."""
     msg.respond('https://ircbot.ocf.berkeley.edu/')
+
+
+def help_macro(bot, msg):
+    """Provide a link to the list of macros."""
+    msg.respond('https://ircbot.ocf.berkeley.edu/macros')
 
 
 def build_request_handler(bot):
@@ -24,24 +30,32 @@ def build_request_handler(bot):
 
     class RequestHandler(http.server.BaseHTTPRequestHandler):
 
+        def render_response(self, template, **context):
+            rendered = jinja_env.get_template(template).render(**context).encode('utf-8')
+            self.send_response(200, 'Okay')
+            self.send_header('Content-Length', len(rendered))
+            self.send_header('Content-Type', 'text/html; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(rendered)
+
         def do_GET(self):
-            if self.path != '/':
-                self.send_response(404, 'File not found')
-                self.end_headers()
-                self.wfile.write(b'404 File not found')
-            else:
+            if self.path == '/':
                 plugins = collections.defaultdict(set)
                 for listener in bot.listeners:
                     plugins[bot.plugins[listener.fn.__module__]].add(listener)
-                rendered = jinja_env.get_template('plugin/templates/help.html').render(
-                    plugins=sorted(plugins.items(), key=lambda p: p[0].__name__),
-                ).encode('utf8')
-
-                self.send_response(200, 'Okay')
-                self.send_header('Content-Length', len(rendered))
-                self.send_header('Content-Type', 'text/html; charset=utf-8')
+                self.render_response(
+                    'plugin/templates/help.html',
+                    plugins=sorted(plugins.items(), key=lambda p: p[0].__name__)
+                )
+            elif self.path == '/macros':
+                self.render_response(
+                    'plugin/templates/macros.html',
+                    macros=bot.plugins['macros'].list(bot)
+                )
+            else:
+                self.send_response(404, 'File not found')
                 self.end_headers()
-                self.wfile.write(rendered)
+                self.wfile.write(b'404 File not found')
 
     return RequestHandler
 
