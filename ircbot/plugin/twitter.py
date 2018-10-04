811fc6cb2bb5e634bdc54ec117cdd36d75615e93
Christopher Cooper <cooperc@ocf.berkeley.edu>
2018-10-04 00:38:40 -0700

    handle 404s and mixed-media tweets

--- ircbot/plugin/twitter.py
+++ ircbot/plugin/twitter.py
@@ -6,7 +6,7 @@ bearer_token = None
 
 
 def register(bot):
-    bot.listen(r'https?://(mobile\.)?twitter\.com/[^/]+/status/([0-9]+)', show_tweet)
+    bot.listen(r'https?://(?:mobile\.|www\.|m\.)?twitter\.com/[^/]+/status/([0-9]+)', show_tweet)
 
 
 def _refresh_token(apikeys):
@@ -19,11 +19,11 @@ def _refresh_token(apikeys):
     resp.raise_for_status()
 
     authorization = resp.json()
-    assert(authorization['token_type'] == 'bearer')
+    assert authorization['token_type'] == 'bearer'
     bearer_token = authorization['access_token']
 
 
-def _tweet(apikeys, status_id):
+def _get_tweet(apikeys, status_id):
     if bearer_token is None:
         _refresh_token(apikeys)
 
@@ -33,19 +33,32 @@ def _tweet(apikeys, status_id):
     ), headers={
         'Authorization': 'Bearer {}'.format(bearer_token),
     })
+    if resp.status_code == 404:
+        return None
     resp.raise_for_status()
 
     return resp.json()
 
 
+def _format_media(media, url):
+    media_urls = [medium['media_url_https']
+                  for medium in media if medium['type'] == 'photo']
+    if any([medium['type'] != 'photo' for medium in media]):
+        media_urls += [url]
+    return ' '.join(media_urls)
+
+
 def _format_tweet(tweet):
     contents = tweet['full_text']
-    media = tweet['extended_entities']['media']
+    media = tweet.get('extended_entities', {}).get('media')
     if media:
+        # url is the same for all media
+        url = media[0]['url']
         contents = contents.replace(
-            media[0]['url'],
-            ' '.join([medium['media_url_https'] for medium in media]),
+            url,
+            _format_media(media, url),
         )
+    contents = contents.replace('\n', ' ')
     return '@{handle} ({realname}): {contents}'.format(
         handle=tweet['user']['screen_name'],
         realname=tweet['user']['name'],
@@ -55,5 +68,7 @@ def _format_tweet(tweet):
 
 def show_tweet(bot, msg):
     """Show tweet content."""
-    tweet = _tweet(bot.twitter_apikeys, msg.match.group(2))
-    msg.respond(_format_tweet(tweet), ping=False)
+    tweet = _get_tweet(bot.twitter_apikeys, msg.match.group(1))
+    if tweet:
+        print(_format_tweet(tweet))
+        msg.respond(_format_tweet(tweet), ping=False)
