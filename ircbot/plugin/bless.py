fa8d3e0d5520d067554f23a9cd2d66e3767c5d00
dongkyunc <mdcha@berkeley.edu>
2018-10-22 12:42:36 -0700

    fixing merge conflict

--- ircbot/plugin/bless.py
+++ ircbot/plugin/bless.py
@@ -1,11 +1,11 @@
 """Blessed be St. Create, progenitor of good code, expeller of bad."""
-
 import random
 
 
 def register(bot):
-    bot.listen(r'^bless$', bless)
-    bot.listen(r'^bless (.*)$', bless_someone)
+    bot.listen(r'bless$', bless)
+    bot.listen(r'bless (.*)$', bless_someone, require_mention=True)
+
 
 def bless(bot, msg):
     msg.respond(random.choice((
@@ -13,9 +13,12 @@ def bless(bot, msg):
         'Not today, Windows.',
         'We are all SysAdmins on this blessed day.',
         'Thou hast made us for thyself, O Server, and our heart is restless until it finds the bug in thee.',
-        'The function of prayer is not to influence the printer, but rather to change the nature of the one who prints.'
+        'The function of prayer is not to influence the printer, but rather to change the nature of the one '
+        'who prints.'
     )))
+
 
 def bless_someone(bot, msg):
     """Have create thank somebody on your behalf."""
-    msg.respond('Blessed be {}, in the name of the compiler, the linker, and the holy runtime!'.format(msg.match.group(1)), ping=False)
+    msg.respond('Blessed be {}, in the name of the compiler, the linker, and the holy runtime!'.format(
+        msg.match.group(1)), ping=False)
