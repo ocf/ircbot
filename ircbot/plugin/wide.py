a7e93f6ba9ca26aef762a6265ed49ef627443fc4
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2018-12-29 07:50:28 -0800

    widetext: some improvements

--- ircbot/plugin/wide.py
+++ ircbot/plugin/wide.py
@@ -1,58 +1,59 @@
-""" Convert characters into full-width text characters"""
+"""Convert characters into full-width text characters."""
 import functools
 from string import ascii_lowercase
 from string import ascii_uppercase
 
-widetext_map = {i: i + 0xFEE0 for i in range(0x21, 0x7F)}
-widetext_map[ord(' ')] = 0x3000  # the space character has unique mapping to become a unicode ideographic space
-space = chr(0x3000)
+WIDETEXT_MAP = {i: i + 0xFEE0 for i in range(0x21, 0x7F)}
+# the space character has unique mapping to become a unicode ideographic space
+WIDETEXT_MAP[ord(' ')] = 0x3000
+SPACE = chr(0x3000)
 
-# As seen in Samuari Jack
-thicc = '卂乃匚刀乇下厶卄工丁长乚从几口尸㔿尺丂丅凵リ山乂丫乙'
+# As seen in Samurai Jack
+THICC = '卂乃匚刀乇下厶卄工丁长乚从几口尸㔿尺丂丅凵リ山乂丫乙'
 
 # Little hack, because translation tables are just dicts
-thicc_map = {
-    **str.maketrans(ascii_lowercase, thicc),
-    **str.maketrans(ascii_uppercase, thicc),
+THICC_MAP = {
+    **str.maketrans(ascii_lowercase, THICC),
+    **str.maketrans(ascii_uppercase, THICC),
 }
 
 
 def register(bot):
-    bot.listen(r'^!w(?: (.*))?', widetext)
-    bot.listen(r'^!w2(?: (.*))?', evenwidertext)
-    bot.listen(r'^!w3(?: (.*))?', superwidetext)
+    bot.listen(r'^!w(.*)?', functools.partial(widetextify, width=0))  # wide text
+    bot.listen(r'^!2w(.*)?', functools.partial(widetextify, width=1))  # even wider text
+    bot.listen(r'^!3w(.*)?', functools.partial(widetextify, width=2))  # super wide text
 
-    bot.listen(r'^!extrathicc(?: (.*))?', extrathicc)
-
-
-def widetextify(bot, msg, width=1):
-    """ｗｅｌｃｏｍｅ　ｔｏ　ｔｈｅ　ｏｃｆ"""
-    text = get_text(bot, msg)
-    if text:
-        response = ''.join(
-            [
-                char.translate(widetext_map) +
-                space * (width - 1) for char in text
-            ],
-        )
-        msg.respond(response, ping=False)
-
-
-widetext = widetextify
-evenwidertext = functools.partial(widetextify, width=2)
-superwidetext = functools.partial(widetextify, width=3)
-
-
-def extrathicc(bot, msg):
-    """乇乂丅尺卂 丅卄工匚匚"""
-    text = get_text(bot, msg)
-    if text:
-        msg.respond(text.translate(thicc_map), ping=False)
+    bot.listen(r'^!thiccen(.*)?', thiccen)
 
 
 def get_text(bot, msg=None):
-    history = bot.recent_messages[msg.channel]
-    return (
-        msg.match.group(1)
-        or history[0][1] if history else None
-    )
+    previous_message = bot.recent_messages[msg.channel]
+
+    if msg.match.group(1).strip():
+        text = msg.match.group(1)
+    elif previous_message:
+        text = previous_message[0][1]
+    else:
+        text = ''
+
+    return text.strip()
+
+
+def widetextify(bot, msg, width):
+    """ｗｅｌｃｏｍｅ　ｔｏ　ｔｈｅ　ｏｃｆ
+
+    These activate either on text supplied after the trigger,
+    or in the event of an unaccompanied trigger, the previous
+    message in the channel.
+    """
+    text = get_text(bot, msg)
+    if text:
+        response = (c.translate(WIDETEXT_MAP) + SPACE * width for c in text)
+        msg.respond(''.join(response), ping=False)
+
+
+def thiccen(bot, msg):
+    """乇乂丅尺卂 丅卄工匚匚"""
+    text = get_text(bot, msg)
+    if text:
+        msg.respond(text.translate(THICC_MAP), ping=False)
