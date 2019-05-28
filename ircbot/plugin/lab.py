46d7651a39551a9fd8681654b3d97c556475b9e3
Minos Park <MrMinos@users.noreply.github.com>
2019-05-28 14:15:41 -0700

    make  work like summoning piiinnngg

--- ircbot/plugin/lab.py
+++ ircbot/plugin/lab.py
@@ -6,7 +6,7 @@ from ocflib.lab.stats import users_in_lab_count
 def register(bot):
     bot.listen(r'is ([a-z]+) in the lab', in_lab, require_mention=True)
     bot.listen(r"(who is|who's) in the lab", who_is_in_lab, require_mention=True)
-    bot.listen(r'(?i)wiitl', who_is_in_lab)
+    bot.listen(r'(?i)w+i+t+l+', who_is_in_lab)
 
 
 def in_lab(bot, msg):
