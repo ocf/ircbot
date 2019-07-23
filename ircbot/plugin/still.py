21cee5d690a1ace8d15e98de24f39a9ccd9cde70
Anthony Sottile <asottile@umich.edu>
2019-07-23 13:04:35 -0700

    docstrings are an antipattern

--- ircbot/plugin/still.py
+++ ircbot/plugin/still.py
@@ -8,6 +8,7 @@ def register(bot):
 
 
 def still(bot, msg):
+    """required docstrings on obvious functions, in 2019 - LOLW"""
     year = datetime.date.today().year
     lol = random.choice(['LOL', 'LOLW', 'LMAO', 'NUUU'])
     msg.respond(f'{msg.match[1]}, in {year} - {lol}!', ping=False)
