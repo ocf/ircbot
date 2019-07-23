942b06f19b0304042b883b5ea25f9b97651398c2
Anthony Sottile <asottile@umich.edu>
2019-07-23 12:31:16 -0700

    Don't ping

--- ircbot/plugin/still.py
+++ ircbot/plugin/still.py
@@ -10,4 +10,4 @@ def register(bot):
 def still(bot, msg):
     year = datetime.date.today().year
     lol = random.choice(['LOL', 'LOLW', 'LMAO', 'NUUU'])
-    msg.respond(f'{msg.match[1]}, in {year} - {lol}!')
+    msg.respond(f'{msg.match[1]}, in {year} - {lol}!', ping=False)
