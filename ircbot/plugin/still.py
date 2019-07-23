b3d0e8d9a3cba2064f2e994cc283712b9626954c
Anthony Sottile <asottile@umich.edu>
2019-07-23 11:42:54 -0700

    Add !still command
    Based on my twitch irc bot's command: https://github.com/anthonywritescode/twitch-chat-bot/blob/e8f3093fb4acb31bd89942c16110f5a8e78f6ea4/bot.py#L160-L166

---
+++ ircbot/plugin/still.py
@@ -0,0 +1,13 @@
+"""Really? it's {current_year}."""
+import datetime
+import random
+
+
+def register(bot):
+    bot.listen(r'^!still (.+)', still)
+
+
+def still(bot, msg):
+    year = datetime.date.today().year
+    lol = random.choice(['LOL', 'LOLW', 'LMAO', 'NUUU'])
+    msg.respond(f'{msg.match[1]}, in {year} - {lol}!')
