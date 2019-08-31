0904a49048f61c96f6beaeb5dda4547789395456
Daniel Kessler <dkessler@berkeley.edu>
2019-08-30 20:10:06 -0700

    google: add !yt command for youtube search
    This adds a layer of abstraction for making general Google searches, and
    adds a youtube function that does the same thing as regular Google but
    adds site:youtube.com to the query.

--- ircbot/plugin/google.py
+++ ircbot/plugin/google.py
@@ -2,32 +2,63 @@
 import requests
 
 
+class GoogleNoResultsError(Exception):
+    pass
+
+
 def register(bot):
     bot.listen(r'!g (.+)$', google)
+    bot.listen(r'!yt (.+)$', youtube)
+
+
+def google_query(key, cx, query):
+    """Searches Google and returns the first result."""
+    resp = requests.get(
+        'https://www.googleapis.com/customsearch/v1',
+        params={
+            'key': key,
+            # The "cx" parameter is the ID of the custom search engine.
+            'cx': cx,
+            'q': query,
+        },
+    )
+
+    resp.raise_for_status()
+
+    results = resp.json()
+    if len(results.get('items', ())) == 0:
+        raise GoogleNoResultsError
+
+    return results['items'][0]
+
+
+def irc_search(bot, msg, query):
+    try:
+        result = google_query(
+            bot.googlesearch_key,
+            bot.googlesearch_cx,
+            query,
+        )
+        msg.respond(
+            f'\x0314{result["title"]} \x0F| \x0302{result["link"]}',
+            ping=False,
+        )
+    except requests.exceptions.HTTPError as e:
+        msg.respond(
+            f'error searching google: {e.response.status_code}',
+            ping=False,
+        )
+    except GoogleNoResultsError:
+        msg.respond('no results :(', ping=False)
+
+
+def youtube(bot, msg):
+    """Search YouTube"""
+    query = msg.match.group(1)
+    return irc_search(bot, msg, query + ' site:youtube.com')
 
 
 def google(bot, msg):
     """Search Google."""
     query = msg.match.group(1)
-
-    resp = requests.get(
-        'https://www.googleapis.com/customsearch/v1',
-        params={
-            'key': bot.googlesearch_key,
-            # The "cx" parameter is the ID of the custom search engine.
-            'cx': bot.googlesearch_cx,
-            'q': query,
-        },
-    )
-    if resp.status_code != 200:
-        msg.respond(f'error searching google: {resp.status_code}', ping=False)
-    else:
-        results = resp.json()
-        if len(results.get('items', ())) == 0:
-            msg.respond('no results :(', ping=False)
-        else:
-            item = results['items'][0]
-            msg.respond(
-                f'\x0314{item["title"]} \x0F| \x0302{item["link"]}',
-                ping=False,
-            )
+    return irc_search(bot, msg, query)
