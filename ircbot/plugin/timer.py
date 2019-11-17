5439dd85236c13b29a354a37b2eacdccc7e5fb09
Minos Park <minos@ocf.berkeley.edu>
2019-11-17 14:31:14 -0800

    modify timer to wait longer periods if a fetch fails, up till max of 30 minutes

--- ircbot/plugin/timer.py
+++ ircbot/plugin/timer.py
@@ -6,15 +6,17 @@ from traceback import format_exc
 
 from ircbot.plugin import debian_security
 
-# Check for Debian security announcements every 5 minutes
-DSA_FREQ = 5
-
 
 def register(bot):
     bot.add_thread(timer)
 
 
 def timer(bot):
+    # Check for Debian security announcements every 5 minutes
+    # If a check fails, we bump add another 5 minutes, until
+    # the maximum of 30 minutes
+    dsa_freq = 5
+
     last_date = None
     last_dsa_check = None
 
@@ -27,11 +29,14 @@ def timer(bot):
             if old and last_date != old:
                 bot.bump_topic()
 
-            if last_dsa_check is None or time.time() - last_dsa_check > 60 * DSA_FREQ:
+            if last_dsa_check is None or time.time() - last_dsa_check > 60 * dsa_freq:
                 last_dsa_check = time.time()
 
                 for line in debian_security.get_new_dsas():
                     bot.say('#rebuild', line)
+
+                # After a successful fetch, we reset timer to 5m
+                dsa_freq = 5
         except Exception:
             error_msg = f'ircbot exception in timer: {format_exc()}'
             bot.say('#rebuild', error_msg)
@@ -47,5 +52,6 @@ def timer(bot):
                     traceback=format_exc(),
                 ),
             )
+            dsa_freq = dsa_freq + 5 if dsa_freq < 30 else dsa_freq
 
         time.sleep(1)
