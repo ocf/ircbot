8a634d474a5039babc62cf3ed7e65f7e29e2b895
Christopher Cooper <cooperc@ocf.berkeley.edu>
2019-11-18 10:47:50 -0800

    move HTTP error handling to timer.py, and don't print traceback

--- ircbot/plugin/timer.py
+++ ircbot/plugin/timer.py
@@ -6,16 +6,20 @@ from traceback import format_exc
 
 from ircbot.plugin import debian_security
 
+# Check for Debian security announcements every 5 minutes
+# If a check fails, we bump add another 5 minutes, until
+# the maximum of 30 minutes
+DSA_FREQ_DEFAULT = 5
+DSA_FREQ_BACKOFF = 5
+DSA_FREQ_MAX = 30
+
 
 def register(bot):
     bot.add_thread(timer)
 
 
 def timer(bot):
-    # Check for Debian security announcements every 5 minutes
-    # If a check fails, we bump add another 5 minutes, until
-    # the maximum of 30 minutes
-    dsa_freq = 5
+    dsa_freq = DSA_FREQ_DEFAULT
 
     last_date = None
     last_dsa_check = None
@@ -36,9 +40,9 @@ def timer(bot):
                     bot.say('#rebuild', line)
 
                 # After a successful fetch, we reset timer to 5m
-                dsa_freq = 5
-        except Exception:
-            error_msg = f'ircbot exception in timer: {format_exc()}'
+                dsa_freq = DSA_FREQ_DEFAULT
+        except Exception as ex:
+            error_msg = f'ircbot exception in timer: {ex}'
             bot.say('#rebuild', error_msg)
             bot.handle_error(
                 dedent(
@@ -52,6 +56,6 @@ def timer(bot):
                     traceback=format_exc(),
                 ),
             )
-            dsa_freq = dsa_freq + 5 if dsa_freq < 30 else dsa_freq
+            dsa_freq = min(dsa_freq + DSA_FREQ_BACKOFF, DSA_FREQ_MAX)
 
         time.sleep(1)
