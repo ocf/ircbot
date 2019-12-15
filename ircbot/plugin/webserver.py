c958a47d4ee31c245cde3cb0be814359e7630b3b
Frank Dai <fydai@ocf.berkeley.edu>
2019-12-14 21:45:49 -0800

    Replace flask dev server with python wsgi server

--- ircbot/plugin/webserver.py
+++ ircbot/plugin/webserver.py
@@ -6,19 +6,24 @@ import os
 from types import ModuleType
 from typing import DefaultDict
 from typing import List
+from typing import Optional
 from typing import Set
 from typing import Tuple
 from typing import TYPE_CHECKING
 
+import cheroot.wsgi
+from flask import abort
 from flask import Flask
 from flask import render_template
 
 if TYPE_CHECKING:
+    from ircbot.ircbot import CreateBot
     from ircbot.ircbot import Listener
 
 app = Flask(__name__)
 
 # Bot plugins, needed for the / route
+create_bot: Optional[CreateBot] = None
 bot_plugins: List[Tuple[ModuleType, Set[Listener]]] = []
 
 
@@ -31,10 +36,17 @@ def route_base():
     global bot_plugins
 
     if not bot_plugins:
+        # Shouldn't happen, the server is started after bot is set
+        if create_bot is None:
+            abort(500)
+
+        # Satisfy mypy
+        bot: CreateBot = create_bot
+
         # Compute and cache the bot's plugins
         bot_plugin_set: DefaultDict[ModuleType, Set[Listener]] = collections.defaultdict(set)
-        for listener in app.bot.listeners:
-            bot_plugin_set[app.bot.plugins[listener.plugin_name]].add(listener)
+        for listener in bot.listeners:
+            bot_plugin_set[bot.plugins[listener.plugin_name]].add(listener)
 
         bot_plugins = sorted(bot_plugin_set.items(), key=lambda p: p[0].__name__)
 
@@ -53,6 +65,9 @@ def route_macros():
 
 
 def start_server(bot):
-    port = os.getenv('HTTP_PORT', 8888)
-    app.bot = bot
-    app.run(host='0.0.0.0', port=int(port))
+    global create_bot
+    port = int(os.getenv('HTTP_PORT', 8888))
+    create_bot = bot
+    d = cheroot.wsgi.PathInfoDispatcher({'/': app})
+    server = cheroot.wsgi.WSGIServer(('0.0.0.0', port), d)
+    server.start()
