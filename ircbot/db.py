167ed5e912695bae99198fd50946cbd519350088
Ethan Smith <ethan@ethanhs.me>
2021-01-25 22:31:06 -0800

    Fix CI

--- ircbot/db.py
+++ ircbot/db.py
@@ -5,16 +5,17 @@ import pymysql
 
 @contextlib.contextmanager
 def cursor(*, user='ocfircbot', password):
-    with contextlib.closing(
-        pymysql.connect(
-            user=user,
-            password=password,
-            db='ocfircbot',
-            host='mysql.ocf.berkeley.edu',
-            cursorclass=pymysql.cursors.DictCursor,
-            charset='utf8mb4',
-            autocommit=True,
-        ),
-    ) as conn:
-        with conn as cursor:
+    conn = pymysql.connect(
+        user=user,
+        password=password,
+        db='ocfircbot',
+        host='mysql.ocf.berkeley.edu',
+        cursorclass=pymysql.cursors.DictCursor,
+        charset='utf8mb4',
+        autocommit=True,
+    )
+    try:
+        with conn.cursor() as cursor:
             yield cursor
+    finally:
+        conn.close()
