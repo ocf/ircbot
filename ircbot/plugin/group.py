b78d1d1f9f0d0f4ad9ce578c562fce291fa55f10
Saurabh Narain <snarain@ocf.berkeley.edu>
2021-04-19 12:01:18 -0700

    changed trigger command and made consistent response format

--- ircbot/plugin/group.py
+++ ircbot/plugin/group.py
@@ -1,6 +1,6 @@
-"""Print group account usernames from subdomain names and vice versa."""
-import re
-
+"""Print group account usernames from domain names and vice versa."""
+from ocflib.account.search import user_exists
+from ocflib.account.search import user_is_group
 from ocflib.vhost.mail import get_mail_vhosts
 from ocflib.vhost.mail import vhosts_for_user
 from ocflib.vhost.web import get_vhosts
@@ -8,60 +8,57 @@ from ocflib.vhost.web import get_vhosts
 
 def register(bot):
     bot.listen(r'group (.*)$', group_lookup)
-    bot.listen(r'subdomain (.*)$', subdomain_lookup)
+    bot.listen(r'domain (.*)$', domain_lookup)
 
 
 def group_lookup(bot, msg):
     user = msg.match.group(1)
-    vhosts = get_vhosts()
-    subdomain = None
-    mail_vhost = vhosts_for_user(user) != set()
-    response = ''
-    for vhost in vhosts.items():
-        if user == vhost[1]['username']:
-            subdomain = vhost[0]
-            continue
-    if subdomain is None and not mail_vhost:
-        msg.respond('A subdomain is not linked to group account ' + user, ping=False)
+    if not user_exists(user):
+        msg.respond('error: account ' + user + ' does not exist', ping=False)
         return
-    elif subdomain is not None and mail_vhost:
-        response = 'Mail and web'
-    elif subdomain is not None and not mail_vhost:
-        response = 'Web'
-    else:
-        subdomain = vhosts_for_user(user)
-        for i in subdomain:
-            subdomain = i[1]
-        response = 'Mail'
-    msg.respond(response + ' virtual hosting for account "' + user + '" is enabled at ' + subdomain, ping=False)
+    msg.respond(group_info(user), ping=False)
 
 
-def subdomain_lookup(bot, msg):
-    subdomain = auto_complete(msg.match.group(1))
-    user = get_vhosts().get(subdomain)
-    mail_vhost_user = None
-    response = ''
-    for mail_vhost in get_mail_vhosts():
-        if mail_vhost[1] == subdomain:
-            mail_vhost_user = mail_vhost[0]
-            continue
-    if user is None and mail_vhost_user is None:
-        msg.respond('A group account is not linked to ' + subdomain, ping=False)
+def domain_lookup(bot, msg):
+    domain = auto_complete(msg.match.group(1))
+    user = get_vhosts().get(domain)
+    if user is not None:
+        user = user['username']
+    if user is None:
+        for mail_vhost in get_mail_vhosts():
+            if mail_vhost[1] == domain:
+                user = mail_vhost[0]
+                continue
+    if user is None:
+        msg.respond('error: domain ' + domain + ' does not exist in vhost.conf or vhost-mail.conf', ping=False)
         return
-    elif user is not None and mail_vhost_user is not None:
-        user = user.get('username')
-        response = 'Mail and web'
-    elif user is not None and mail_vhost_user is None:
-        user = user.get('username')
-        response = 'Web'
-    else:
-        user = mail_vhost_user
-        response = 'Mail'
-    msg.respond(response + ' virtual hosting at ' + subdomain + ' is enabled for account "' + user + '"', ping=False)
+    msg.respond(group_info(user), ping=False)
+
+
+def group_info(user):
+    web_vhosts = get_vhosts()
+    web_domains = []
+    mail_vhosts = vhosts_for_user(user)
+    mail_domains = []
+    for web_vhost in web_vhosts.items():
+        if user == web_vhost[1]['username']:
+            web_domains.extend([web_vhost[0]] + web_vhost[1]['aliases'])
+            continue
+    for i in mail_vhosts:
+        mail_domains.extend([i[1]])
+    if web_domains == []:
+        web_domains = ['none']
+    if mail_vhosts == set():
+        mail_domains = ['none']
+    user_info_str = 'account: ' + user
+    is_group_account_str = ' | is group account: ' + str.lower(str(user_is_group(user)))
+    web_domains_str = ' | web vhosts: ' + ', '.join(web_domains)
+    mail_domains_str = ' | mail vhosts: ' + ', '.join(mail_domains)
+    return user_info_str + is_group_account_str + web_domains_str + mail_domains_str
 
 
 def auto_complete(s):
     valid_endings = ['.berkeley.edu', '.org', '.com', '.edu']
-    if not any([re.search(valid_ending, s) is not None for valid_ending in valid_endings]):
-        return s + '.berkeley.edu'
-    return s
+    if any([s.rfind(valid_ending) != -1 for valid_ending in valid_endings]):
+        return s
+    return s + '.berkeley.edu'
